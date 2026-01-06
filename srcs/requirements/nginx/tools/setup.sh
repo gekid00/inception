af916c9287b282add762c86770bca4b1c49ee200
gekid00 <>
2026-01-06 16:28:41 +0100

    Fix nginx server_name and SSL cert CN

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -6,7 +6,7 @@ if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout /etc/nginx/ssl/nginx.key \
         -out /etc/nginx/ssl/nginx.crt \
-        -subj "/C=FR/ST=France/L=Paris/O=42/OU=42/CN=login.42.fr"
+        -subj "/C=FR/ST=France/L=Paris/O=42/OU=42/CN=${DOMAIN_NAME}"
 fi
 
 # Lancer Nginx
